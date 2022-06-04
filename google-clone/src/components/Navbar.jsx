456ccf67cd68c6e28248ffef57cd5a135afbb894
OverZone-Production <supervegito835@example.com>
2022-06-04 13:36:40 +0700

    ep6

--- google-clone/src/components/Navbar.jsx
+++ google-clone/src/components/Navbar.jsx
@@ -1,40 +1,34 @@
-import "../assets/CSS/google.css"
-import appGrid from "../assets/img/grid_icon.svg"
+import "../assets/CSS/google.css";
+import appsGrid from "../assets/img/grid_icon.svg";
 
-const MenuItem = (prop) => {
-
-    return (
-        <a href="">
-            <div className="menu-item">{prop.content}</div>
-        </a>
-    )
-}
+const MenuItem = (props) => {
+  return (
+    <a href="/">
+      <div className="menu-item">{props.content}</div>
+    </a>
+  );
+};
 
 const RenderMenuItems = () => {
-    const menu = [
-        {content: "Gmail"},
-        {content: "Images"},
-        {content: (
-                <img classname='icon'src={appGrid} alt='icon' />
-            ),
-        },
-        // {content: (
-        //     <img classname='avatar'src={avatarImage} alt='icon' />
-        // ),
-        {content: "Avatar"},
-    
-    ]
+  const menu = [
+    { content: "Gmail" },
+    { content: "Images" },
+    {
+      content: <img className="icon" src={appsGrid} alt="icon" />,
+    },
+    { content: <img className="avatar" src="#" alt="avatar" /> },
+  ];
 
-    return menu.map(m => <MenuItem content={m.content} />)
-}
- 
-// ES6
-const Navbar = () => { // => Arrow Function
-    return (
-        <div className="navbar-comp">
-            <RenderMenuItems />
-        </div>
-    )
-}
+  return menu.map((m, idx) => <MenuItem key={idx} content={m.content} />);
+};
+
+// ES6 - Arrow function
+const Navbar = () => {
+  return (
+    <div className="navbar-comp">
+      <RenderMenuItems />
+    </div>
+  );
+};
 
 export default Navbar;
